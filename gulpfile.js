de93b54347d3d46b024df14a6b4cec01a10ee2d9
jayhung97724 <jayhung97724@gmail.com>
2016-12-22 16:41:24 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var paths = {
+  src: {
+   less  : './src/style/less/*.less',
+   css   : './src/style/css/*.css',
+   js    : './src/js/*.js',
+   pug   : './src/pug/*.pug',
+   images: './src/img/**'
+ },
+ dest: {
+   html  : './dest',
+   css   : './dest/style',
+   js    : './dest/js',
+   images: './dest/assets/img'
+
+ }
+};
+
+gulp.task('pug', function() {
+  gulp.src(paths.src.pug)
+      .pipe($.pug())
+      .pipe(gulp.dest('./dest'));
+});
+
+gulp.task('less', function() {
+  gulp.src(paths.src.less)
+      .pipe($.less())
+      .pipe(gulp.dest(paths.dest.css));
+});
+
+gulp.task('scripts', function() {
+  gulp.src(paths.src.js)
+      .pipe($.uglify())
+      .pipe(gulp.dest(paths.dest.js))
+})
+
+gulp.task('images', function() {
+  gulp.src(paths.src.images)
+      .pipe($.imagemin())
+      .pipe(gulp.dest(paths.dest.images))
+})
+
+gulp.task('webserver', function(){
+  gulp
+    .src(paths.dest.html)
+    .pipe($.webserver({
+      port: 8080,
+      livereload: true,
+      directoryListing: false
+    }));
+})
+
+gulp.task('watch',function(){
+  gulp.watch(paths.src.pug, ['pug'])
+  gulp.watch(paths.src.less, ['less']);
+  gulp.watch(paths.src.js, ['scripts']);
+});
+
+gulp.task('default', ['pug', 'less', 'scripts', 'webserver', 'watch']);
