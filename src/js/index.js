7b7895bea2b65ee2b2fd358c689debd09913c422
jd615645 <jd615645@gmail.com>
2017-01-19 18:31:42 +0800

    :cat2: add slide

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
   $('.honeycombs').honeycombs();
+
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: true,
+    items: 1,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true
+  });
 });
