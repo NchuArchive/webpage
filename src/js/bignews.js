1c6db7d9cc8e8fbfd5e23bde29d8ba1c144030fb
jd615645 <jd615645@gmail.com>
2017-01-22 16:39:28 +0800

    :cat2:add bignews

---
+++ src/js/bignews.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  var period = ['台灣總督府時期', '省立農學院時期', '國立中興大學時期'];
+
+  var bignews = new Vue({
+    el: '#bigNews',
+    data: {
+      periods: period[0],
+      view: 0,
+      japan: [
+        {
+          title: 'test',
+          context: 'test text',
+          date: '2016/01/01'
+        },
+        {
+          title: '1',
+          context: '2',
+          date: '2016/01/02'
+        }
+      ]
+    },
+    methods: {
+      switchPeriods: function(item) {
+        this.periods = period[item];
+        this.view = item;
+      }
+    }
+  });
+});
