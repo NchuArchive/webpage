72611326422a8889cc5485d9ad4ccc31157ea11c
jd615645 <jd615645@gmail.com>
2017-02-09 13:29:05 +0800

    :cat2:更改大事紀時期&將切換時期改為computed

--- src/js/bignews.js
+++ src/js/bignews.js
@@ -1,37 +1,38 @@
-$(document).ready(function() {
-  var period = ['台灣總督府時期', '省立農學院時期', '國立中興大學時期'];
-  var api = '19ElFzY33pZJbx_ICQW9PhtliXZG9QjzXdO_8Dl5O-5c';
-  var apiUrl = 'http://gsx2json.com/api?id=' + api + '&sheet=';
-  var timeline = [];
+var bignews = new Vue({
+  el: '#bigNews',
+  data() {
+    return {
+      view: 0,
+      selectPeriods: null,
+      timelins: {},
+    }
+  },
+  computed: {
+    timeline() {
+      return _.get(this.timelins, [this.selectPeriods], []);
+    }
+  },
+  mounted() {
+    var api = '19ElFzY33pZJbx_ICQW9PhtliXZG9QjzXdO_8Dl5O-5c',
+        apiUrl = 'http://gsx2json.com/api?id=' + api + '&sheet=';
+    var googleSheet = [];
 
-  var bignews = new Vue({
-    el: '#bigNews',
-    data() {
-      return {
-        periods: period[0],
-        view: 0,
-        timeline: [],
-      }
-    },
-    mounted() {
-      var a1 = $.getJSON(apiUrl + '1'),
-          a2 = $.getJSON(apiUrl + '2'),
-          a3 = $.getJSON(apiUrl + '3');
+    for (var i = 1; i <= 5; i++) {
+      googleSheet.push($.getJSON(apiUrl + i));
+    }
+    $.when
+      .apply($, googleSheet)
+      .then((...careerData) => {
+        var periodTitle = ['台灣總督府時期', '台北帝國大學時期', '台灣總督府台中農林學校時期(台中)', '省立農學院時期', '國立中興大學時期'];
+        $.each(careerData, (key, val) => {
+          var title = periodTitle[key],
+              news = val[0]['rows'];
 
-      var my = this;
-      $.when(a1, a2, a3).done(function(r1, r2, r3) {
-        timeline.push(r1[0]['rows']);
-        timeline.push(r2[0]['rows']);
-        timeline.push(r3[0]['rows']);
-        my.timeline = timeline[0];
+          _.setWith(this.timelins, [key], {'title': title, 'news': news}, Object);
+        });
+        this.selectPeriods = 0;
       });
-    },
-    methods: {
-      switchPeriods: function(item) {
-        this.periods = period[item];
-        this.view = item;
-        this.timeline = timeline[item];
-      }
-    },
-  });
+  },
+  methods: {
+  },
 });
