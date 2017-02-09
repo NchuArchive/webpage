3e6217b1ab730838b09b34eb3b73198e9536c616
jd615645 <jd615645@gmail.com>
2017-02-09 16:59:12 +0800

    :cat2:增加交通資訊

---
+++ src/js/visit.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  var map = L.map('map').setView([24.123247, 120.675683], 17);
+
+  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);
+
+  L.marker([24.123247, 120.675683]).addTo(map);
+});
